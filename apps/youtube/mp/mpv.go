15371393a81db4a678f71fa3619bc6dbe261ef7c
thangnd85 <thangnd85@gmail.com>
2021-04-24 21:42:32 +0700

    Update mpv.go

--- apps/youtube/mp/mpv.go
+++ apps/youtube/mp/mpv.go
@@ -23,8 +23,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/aykevl/plaincast/config"
-	"github.com/aykevl/plaincast/log"
+	"github.com/thangnd85/plaincast/config"
+	"github.com/thangnd85/plaincast/log"
 )
 
 var MPV_PROPERTY_UNAVAILABLE = errors.New("mpv: property unavailable")
